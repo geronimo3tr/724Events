6604c53a5f48b065cbef6d5966f19a51295cccca
geronimo3tr <jocelyn.lepagepro@gmail.com>
2024-01-16 15:26:11 +0100

    ancrrs+ error

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -37,7 +37,7 @@ const Slider = () => {
           <div className="SlideCard__paginationContainer">
             <div className="SlideCard__pagination">
               {byDateDesc.map((_, radioIdx) => (
-                <input key={`${event.id}`} type="radio" name="radio-button" checked={index === radioIdx} />
+                <input key={event.id} type="radio" name="radio-button" checked={index === radioIdx} readOnly />
               ))}
             </div>
           </div>
